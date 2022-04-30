b4df70d2f35a442a09feffd8ac3b9108268bd8a7
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-04-30 12:34:01 +0430

    Add migration for create scope table

---
+++ migrations/1651305184393-createScopeTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class createScopeTable1651305184393 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS scope
+                (
+                    id integer NOT NULL,
+                    label character varying COLLATE pg_catalog."default" NOT NULL,
+                    description character varying COLLATE pg_catalog."default" NOT NULL,
+                    CONSTRAINT "PK_d3425631cbb370861a58c3e88c7" PRIMARY KEY (id)
+                )
+`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE IF EXISTS scope`)
+    }
+
+}
