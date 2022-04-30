65142aebce741b6042ba6d50d30f27cf7cc20ce9
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-04-30 12:33:29 +0430

    Add migration for create application table

---
+++ migrations/1651304945528-createApplicationTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class createApplicationTable1651304945528 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS application
+                (
+                    id integer NOT NULL,
+                    label character varying COLLATE pg_catalog."default" NOT NULL,
+                    name character varying COLLATE pg_catalog."default" NOT NULL,
+                    secret character varying COLLATE pg_catalog."default" NOT NULL,
+                    scopes text[] COLLATE pg_catalog."default" NOT NULL DEFAULT '{}'::text[],
+                    "validIps" text[] COLLATE pg_catalog."default" NOT NULL DEFAULT '{}'::text[],
+                    logo character varying COLLATE pg_catalog."default" NOT NULL,
+                    "allowedRequestsPerHour" integer NOT NULL,
+                    "isActive" boolean NOT NULL,
+                    "organizationId" integer,
+                    CONSTRAINT "PK_569e0c3e863ebdf5f2408ee1670" PRIMARY KEY (id),
+                    CONSTRAINT "FK_88e675c3f80602005b728979e4a" FOREIGN KEY ("organizationId")
+                        REFERENCES public.organization (id) MATCH SIMPLE
+                        ON UPDATE NO ACTION
+                        ON DELETE NO ACTION
+                )
+
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.query(`DROP TABLE IF EXISTS application`)
+
+    }
+
+}
