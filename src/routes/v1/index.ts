1021b61bdbc585eb6eccd79fcb801873fc309528
Carlos Quintero <cquinterom096@outlook.com>
2022-05-11 08:42:15 -0500

    add logout router

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { authenticationRouter } from './authenticationRouter';
 import { authorizationRouter } from './authorizationRouter';
 import { healthRouter } from './healthRouter';
+import { logoutRouter } from './logoutRouter';
 import { nonceRouter } from './nonceRouter';
 
 export const v1Router = express.Router();
@@ -9,3 +10,4 @@ v1Router.use('/v1', healthRouter);
 v1Router.use('/v1', nonceRouter);
 v1Router.use('/v1', authorizationRouter);
 v1Router.use('/v1', authenticationRouter);
+v1Router.use('/v1', logoutRouter);
