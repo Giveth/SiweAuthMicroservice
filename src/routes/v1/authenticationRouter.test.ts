25c4d4deef4284f1a7ff63d89f9b20b9389121d6
Carlos Quintero <cquinterom096@outlook.com>
2022-05-16 00:33:15 -0500

    add ethers and deploy scripts

--- src/routes/v1/authenticationRouter.test.ts
+++ src/routes/v1/authenticationRouter.test.ts
@@ -9,19 +9,25 @@ import { generateNonce } from 'siwe';
 import moment from 'moment';
 import sinon from 'sinon';
 import { SiweMessage } from 'siwe';
+import { ethers } from 'ethers';
+
 const siwe = require('siwe');
+const privateKey = process.env.PRIVATE_ETHERS_TEST_KEY as string;
+const provider = ethers.getDefaultProvider();
 
 describe('/authentication test cases', authenticationTestCases);
 
 function authenticationTestCases() {
   const domain = 'localhost';
-  const origin = 'https://localhost/v1/authentication';
+  const origin = 'https://giveth.io';
 
   it('should return the public address, jwt and expiration', async () => {
     const nonce = await SiweNonce.create({
       nonce: generateNonce(),
       expirationDate: moment().add(5, 'minutes'),
     }).save();
+
+    const wallet = new ethers.Wallet(privateKey, provider);
 
     const address = generateRandomEthereumAddress();
     const siweMessage = new siwe.SiweMessage({
@@ -34,11 +40,16 @@ function authenticationTestCases() {
       chainId: '1',
     });
 
+    const textMessage = siweMessage.prepareMessage()
+    const signature = await wallet.signMessage(textMessage);
+    // const splitted = ethers.utils.splitSignature(signature)
+
+    // for future stubbing examples
     sinon.stub(SiweMessage.prototype, 'validate').resolves(siweMessage);
     const result = await axios.post(`${serverUrl}/v1/authentication`, {
-      message: siweMessage.prepareMessage(),
+      message: textMessage,
       nonce: nonce.nonce,
-      signature: '0xc993a34ff3261732402576873a2e8e0983a5dc03',
+      signature: signature,
     });
     assert.equal(result.status, 200);
     assert.equal(result.data.publicAddress, address);
