3e406999d1ebf6a99f96153a613113fde3a53bb9
Carlos <carlos.quintero096@gmail.com>
2023-10-04 18:16:19 -0500

    fix migration file for approvalExpirationDate

--- migrations/1696397559095-AddExpirationDateResetToMultisigSession.ts
+++ migrations/1696397559095-AddExpirationDateResetToMultisigSession.ts
@@ -4,14 +4,22 @@ export class AddExpirationDateResetToMultisigSession1696397559095
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      'multisig_session',
-      new TableColumn({
-        name: 'approvalExpirationDate',
-        type: 'date',
-        isNullable: true,
-      }),
+    const table = await queryRunner.getTable('multisig_session');
+    if (!table) return;
+
+    const approvalExpirationDateColumnExists = table.columns.some(
+      c => c.name === 'approvalExpirationDate',
     );
+    if (!approvalExpirationDateColumnExists) {
+      await queryRunner.addColumn(
+        'multisig_session',
+        new TableColumn({
+          name: 'approvalExpirationDate',
+          type: 'date',
+          isNullable: true,
+        }),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
