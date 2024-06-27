86dbd6ee7307c024df553b001a70c412f86b2665
Ali Ebrahimi <65724329+ae2079@users.noreply.github.com>
2024-06-27 06:36:28 +0330

    Merge pull request #28 from Giveth/10-track-count-of-multisig-sessions
    Add multisigSessionCountRouter

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -7,6 +7,7 @@ import { nonceRouter } from './nonceRouter';
 import { passportNonceRouter } from '@/src/routes/v1/passportNonceRouter';
 import { passportAuthenticationRouter } from '@/src/routes/v1/passportAuthenticationRouter';
 import { multisigAuthenticationRouter } from './multisigAuthenticationRouter';
+import { multisigSessionCountRouter } from '@/src/routes/v1/multisigSessionCountRouter';
 
 export const v1Router = express.Router();
 v1Router.use('/v1', healthRouter);
@@ -15,6 +16,7 @@ v1Router.use('/v1', authorizationRouter);
 v1Router.use('/v1', authenticationRouter);
 v1Router.use('/v1', logoutRouter);
 v1Router.use('/v1', multisigAuthenticationRouter);
+v1Router.use('/v1', multisigSessionCountRouter);
 
 // Passport routes
 v1Router.use('/v1', passportNonceRouter);
