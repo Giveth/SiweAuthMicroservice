b43add23a49fa1f02cc6aa06bd942b2398031157
Ramin <raminramazanpour@gmail.com>
2025-10-09 20:15:01 +0330

    more and more headers

--- src/services/safeServices.ts
+++ src/services/safeServices.ts
@@ -31,19 +31,34 @@ export const fetchSafeMessageByTimestamp = async (
   networkId: number,
 ) => {
   let safeMessage;
-  logger.info('fetchSafeMessageByTimestamp()', {
+  logger.info('fetchSafeMessageByTimestamp() info:', {
     safeAddress,
     safeMessageTimestamp,
     networkId,
+    date: new Date().toISOString(),
   });
   try {
     const response = await axios.get(
       `https://safe-client.safe.global/v1/chains/${networkId}/safes/${safeAddress}/messages`,
       {
         headers: {
-          'Content-Type': 'application/json',
+          accept: 'application/json',
+          'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
+          'cache-control': 'no-cache',
+          dnt: '1',
+          pragma: 'no-cache',
+          priority: 'u=1, i',
+          referer: 'https://safe-client.safe.global/api',
+          'sec-ch-ua':
+            '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'sec-gpc': '1',
           'user-agent':
-            'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
         },
       },
     );
