69865bd4ba6389c250a6feafc1ff0509c14f25d7
Ramin <raminramazanpour@gmail.com>
2026-06-11 13:12:04 +0330

    Refactor import order in server.ts for improved readability

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
+import bodyParser from 'body-parser';
 import express, { Application } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import { v1Router } from './routes/v1';
-import { AppDataSource } from './dataSource';
 import { DataSource } from 'typeorm';
-import bodyParser from 'body-parser';
+import { AppDataSource } from './dataSource';
 import { errorHandler } from './middlewares/errorHandler';
+import { v1Router } from './routes/v1';
 import { adminJsRootPath, getAdminBroRouter } from './routes/v1/adminbroRouter';
 import { logger } from './utils/logger';
 const cors = require('cors');
